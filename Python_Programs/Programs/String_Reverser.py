357f9e00e473fe56c63b2fa8202a3591c94ee325
jbbranch9 <jacob.b.branch1@gmail.com>
2018-10-07 14:44:58 -0700

    added python programs page

---
+++ Python_Programs/Programs/String_Reverser.py
@@ -0,0 +1,14 @@
+print("Enter a string:") #input the string
+string = input()
+
+length = len(string) #define length
+z = length #z is location of last character
+revstring = "!" #define reversed string using a dummy character
+
+while z > 0:
+    z = (z-1) #increment the loop
+    last = string[z] #create a substring from the last character
+    revstring = (revstring+last) #add substring to end of reversed string
+
+revstring = (revstring[1:(length+1)]) #remove dummy character
+print(revstring)
