27fca2605b7f94f360ab57ab5cfb884aa48f2a38
jbbranch9 <jacob.b.branch1@gmail.com>
2018-10-18 20:54:56 -0700

    integrated word bank into hangman.py

--- Python_Programs/Programs/Hangman/old/hangman_old.py
+++ Python_Programs/Programs/Hangman/old/hangman_old.py
@@ -1,41 +1,13 @@
-#import modules
 import os
 def cls():
     os.system('cls' if os.name=='nt' else 'clear') #import screen clearing function from os
 
-import word #word database, sorted by difficulty
-
-import random
-
-def get_word():
-    #setup global variables
-    global lvl
-    global word
-    
-    #prompt for difficulty level
-    lvl = 0
-    while lvl < 1 or lvl > 50:
-    
-        lvl = int(input("Choose difficulty (1-50): "))
-    
-    lower_bound = (lvl-1)*85
-    upper_bound = 84+lower_bound
-    if lvl == 50:
-        upper_bound = 4234
-    
-    index = random.randint(lower_bound, upper_bound)
-    word = word.bank[index]
-    
 #initial conditions
-get_word()
 
+word = "bookkeeper" #get word
 lng = len(word)
 
 strike = 0
-if lvl < 26:
-    max_strike = 7
-else:
-    max_strike = 10
 
 displist = [] #display list of characters representing the word in progress
 dispstr = "" #string comprised of current characters in above list
@@ -50,7 +22,7 @@ while dit < lng:
 #print(displist)
 print(dispstr)
 
-while new < lng and strike < max_strike:
+while new < lng and strike < 5:
 
     print("Guess a letter:")
     ltr = input()
@@ -91,13 +63,11 @@ cls()
 print(dispstr)
 print("")
 
-if strike < max_strike and new == lng:
+if strike < 5 and new == lng:
     print("You Win!")
 
 else:
     print("Game Over!")
-
-print(word)
-
+    
 input()
 
