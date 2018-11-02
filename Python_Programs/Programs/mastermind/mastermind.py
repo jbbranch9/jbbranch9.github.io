99f462b6f540701376da195b63e3cff9d7d64ec0
jbbranch9 <jacob.b.branch1@gmail.com>
2018-11-02 01:03:46 -0700

    Update Python_Programs/Programs/mastermind/mastermind.py

--- Python_Programs/Programs/mastermind/mastermind.py
+++ Python_Programs/Programs/mastermind/mastermind.py
@@ -11,6 +11,7 @@ G = ["G", "g", "GREEN", "green", "Green", "G.", "g.", "GREEN.", "green.", "Green
 B = ["B", "b", "BLUE", "blue", "Blue", "B.", "B.", "BLUE.", "blue.", "Blue.", "B!", "b!", "BLUE!", "blue!", "Blue!"]
 W = ["W", "w", "WHITE", "white", "White", "W.", "w.", "WHITE.", "white.", "White.", "W!", "w!", "WHITE!", "white!", "White!"]
 
+#prompts player for color
 def get_color(prompt):
     color = ""
     while color != "R" and color != "G" and color != "B" and color != "W":
@@ -26,9 +27,11 @@ def get_color(prompt):
         elif color in W:
             color = "W"
         else:
-            print("Input error, try typing just the first letter.")
+            print("Input error. Try again.")
+            print("")
     return color
 
+#this generates the code that the players must crack
 def generate_code():
     code = []
     for i in range(4):
@@ -43,6 +46,7 @@ def generate_code():
             code.append("W")
     return code
 
+#using the get_color() prompt, this builds a list of the players 4 guesses
 def prompt_player():
     guess = []
     for i in range(4):
@@ -68,6 +72,7 @@ def color_count(list):
         count_list.append(count)
     return count_list
 
+#checks for color matches between the code and guess lists, returns a number between 0-4
 def check_color(code_counts, guess_counts):
     matches = 0
     for i in range(len(code_counts)):
@@ -77,7 +82,7 @@ def check_color(code_counts, guess_counts):
             matches = matches + int(code_counts[i])
     return int(matches)
         
-#returns the number of exact position/color matches
+#returns the number of exact position/color matches, returns a number between 0-4
 def check_position(list1, list2):
     matches = 0
     for i in range(len(list1)):
@@ -85,14 +90,15 @@ def check_position(list1, list2):
             matches += 1
     return int(matches)
 
+#this clears the screen, and refreshes the list of past guesses each round
 def refresh_screen(round, archive):
     cls()
     print("------------------Mastermind------------------")
     print("")
-    print("Round:", round + 1)
+    print("Round:", round, "/ 10")
     print("")
 
-    if round !=0:
+    if round !=1:
         print("Previous Guesses:")
         print("")
         print("Peg 1---Peg 2---Peg 3---Peg 4---Exact---Color")
@@ -107,9 +113,10 @@ def refresh_screen(round, archive):
         print("")
 
 def main():
+    #initial values
     position_match = 0
     color_match = 0
-    round = 0
+    round = 1
     code = generate_code()
     guess = ""
     archive = []
@@ -118,33 +125,41 @@ def main():
     This is the "cheat" for debugging.
     It shows the code at the beginning
     of the game if not commented out.
-    cls()
-    print(code)
-    input()
     """
+##    cls()
+##    print(code)
+##    input()
     
-    while round <= 9 and guess != code:
+    while round <= 10 and guess != code:
         refresh_screen(round, archive)
-                
+        
+        #prompt player for guess, add guess to archive list
         guess = prompt_player()
         archive.extend(guess)
         
+        #check for position/color matches, add them to archive list
         position_match = check_position(code, guess)
         archive.extend(str(position_match))
-        
+                
+        #check for color only matches, add them to archive list
+        #note that the number of position AND color matches is subtracted from the total color matches to get the color ONLY matches
         color_match = check_color(color_count(code), color_count(guess)) - position_match
         archive.extend(str(color_match))
          
         round += 1
-        
-    refresh_screen(9, archive)
+    
+    #this is the end game procedure
+    refresh_screen(round - 1, archive)
     if guess == code:
-        print("Congratulations, you cracked the code!")
+        print("Congratulations, you cracked the code in", round - 1, "rounds!")
     else:
         print("Game over! Better luck next time.")
+        print("")
+        print("The code was:", code)
     print("")
     print("Press 'enter' to replay!")
 
+#call and repeat main() until program is closed
 while True:
     main()
     input()
