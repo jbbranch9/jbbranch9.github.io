a1223bd2f3bad80e3f67bcec3e8e54d56f0b7f9d
jbbranch9 <jacob.b.branch1@gmail.com>
2018-11-01 23:26:09 -0700

    Update Python_Programs/Programs/mastermind/mastermind.py

--- Python_Programs/Programs/mastermind/mastermind.py
+++ Python_Programs/Programs/mastermind/mastermind.py
@@ -1,5 +1,10 @@
 from random import randint
 
+#import/define screen clearing function
+import os
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
 #defining accepted inputs
 R = ["R", "r", "RED", "red", "Red", "R.", "r.", "RED.", "red.", "Red.", "R!", "r!", "RED!", "red!", "Red!"]
 G = ["G", "g", "GREEN", "green", "Green", "G.", "g.", "GREEN.", "green.", "Green.", "G!", "g!", "GREEN!", "green!", "Green!"]
@@ -71,7 +76,6 @@ def check_color(code_counts, guess_counts):
             matches = matches + int(code_counts[i])
     return int(matches)
         
-        
 #returns the number of exact position/color matches
 def check_position(list1, list2):
     matches = 0
@@ -80,27 +84,52 @@ def check_position(list1, list2):
             matches += 1
     return int(matches)
 
+def refresh_screen(round, archive):
+    cls()
+    print("Round:", round + 1)
+    print("Previous Guesses:")
+    for i in range(int((len(archive)+1)/6)):
+        print(archive[(6*i):(6*i)+6])
+    
+
 def give_feedback():
     return "undefined" 
 
 def main():
     position_match = 0
     color_match = 0
-    
+    round = 0
     code = generate_code()
-    print(code)
+    #print(code)
+    guess = ""
+    archive = []
     
-    print(color_count(code))
+    while round <= 9 and guess != code:
+        if round != 0:
+            refresh_screen(round, archive)
+                
+        guess = prompt_player()
+        archive.extend(guess)
+        print(guess)
+
+        
+        position_match = check_position(code, guess)
+        archive.extend(str(position_match))
+        
+        color_match = check_color(color_count(code), color_count(guess)) - position_match
+        archive.extend(str(color_match))
+        
+        print(archive)
+        
+        print("Position and Color:", position_match, "Color Only:", color_match)
     
-    guess = prompt_player()
-    print(guess)
-    
-    position_match = check_position(code, guess)
-    color_match = check_color(color_count(code), color_count(guess)) - position_match
-    
-    print("Position and Color:", position_match, "Color Only:", color_match)
-    
-    
+        round += 1
+
+    if guess == code:
+        print("Congratulations, you cracked the code!")
+    else:
+        print("Game over! Better luck next time.")
+
 
 
 
