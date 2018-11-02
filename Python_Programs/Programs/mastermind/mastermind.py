3bc75eb45eaecda7851ef715ff05a6d23405b500
jbbranch9 <jacob.b.branch1@gmail.com>
2018-11-01 22:40:08 -0700

    Update Python_Programs/Programs/mastermind/mastermind.py

--- Python_Programs/Programs/mastermind/mastermind.py
+++ Python_Programs/Programs/mastermind/mastermind.py
@@ -6,10 +6,11 @@ G = ["G", "g", "GREEN", "green", "Green", "G.", "g.", "GREEN.", "green.", "Green
 B = ["B", "b", "BLUE", "blue", "Blue", "B.", "B.", "BLUE.", "blue.", "Blue.", "B!", "b!", "BLUE!", "blue!", "Blue!"]
 W = ["W", "w", "WHITE", "white", "White", "W.", "w.", "WHITE.", "white.", "White.", "W!", "w!", "WHITE!", "white!", "White!"]
 
-def get_color():
+def get_color(prompt):
     color = ""
     while color != "R" and color != "G" and color != "B" and color != "W":
-        color = input("Choose a color (R=Red, G=Green, B=Blue, or W=White): ")
+        print("Choose a color for slot", prompt)
+        color = input("(R=Red, G=Green, B=Blue, or W=White): ")
         if color in R:
             color = "R"
         elif color in G:
@@ -36,5 +37,74 @@ def generate_code():
             code.append("W")
     return code
 
-code = generate_code()
-print(code)
+def prompt_player():
+    guess = []
+    for i in range(4):
+        color = get_color(i + 1)
+        guess.append(color)
+    return guess
+
+"""
+This function counts occurrences of each
+color within the inputted list and returns
+the count in a list of the format:
+[# red, # green, # blue, # white]
+"""
+def color_count(list):
+    all_colors = ["R", "G", "B", "W"]
+    count_list = []
+    for j in range(len(all_colors)):
+        color = all_colors[j]
+        count = 0
+        for i in range(len(list)):
+            if list[i] == color:
+                count = count + 1
+        count_list.append(count)
+    return count_list
+
+def check_color(code_counts, guess_counts):
+    matches = 0
+    for i in range(len(code_counts)):
+        if code_counts[i] >= guess_counts[i]:
+            matches = matches + int(guess_counts[i])
+        else:
+            matches = matches + int(code_counts[i])
+    return int(matches)
+        
+        
+#returns the number of exact position/color matches
+def check_position(list1, list2):
+    matches = 0
+    for i in range(len(list1)):
+        if list1[i] == list2[i]:
+            matches += 1
+    return int(matches)
+
+def give_feedback():
+    return "undefined" 
+
+def main():
+    position_match = 0
+    color_match = 0
+    
+    code = generate_code()
+    print(code)
+    
+    print(color_count(code))
+    
+    guess = prompt_player()
+    print(guess)
+    
+    position_match = check_position(code, guess)
+    color_match = check_color(color_count(code), color_count(guess)) - position_match
+    
+    print("Position and Color:", position_match, "Color Only:", color_match)
+    
+    
+
+
+
+
+while True:
+    main()
+    input()
