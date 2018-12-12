d3191e5dfa1e7d973040dab114ad77fef8c3e2f3
jbbranch9 <jacob.b.branch1@gmail.com>
2018-12-11 22:50:05 -0800

    finished robot treasure hunt

--- CS160_Coursework/robot_treasure_hunt/robot_treasure_hunt.py
+++ CS160_Coursework/robot_treasure_hunt/robot_treasure_hunt.py
@@ -1,52 +1,39 @@
-import random
-
-def algorithm_one(energy_position, zb_position, zb_movement):
-    zb_direction = 1
+def algorithm_one(energy_position, zb_position, zb_movement, zb_direction):
     total_movement_algorithm_one = 0
     while zb_position != energy_position:
-        zb_position += (zb_movement * zb_direction)
-        total_movement_algorithm_one += zb_movement
+        for i in range (zb_movement):
+            zb_position += (zb_direction)
+            total_movement_algorithm_one += zb_movement
+            if zb_position == energy_position:
+                break
+        if zb_position == energy_position:
+            break
         zb_direction = -(zb_direction)
-        zb_movement += 1  
+        zb_movement += 250  
     return total_movement_algorithm_one
-        
-def algorithm_two_positive_first(energy_position, zb_position, zb_movement):
-    zb_direction = 1
-    total_movement_algorithm_two = 0
-    while zb_position != energy_position:
-        zb_position += zb_direction
-        total_movement_algorithm_two += zb_movement
-        if zb_position == -1000 or zb_position == 1000:
-            zb_direction = -(zb_direction)      
-    return total_movement_algorithm_two
 
-def algorithm_two_negative_first(energy_position, zb_position, zb_movement):
-    zb_direction = -1
+def algorithm_two(energy_position, zb_position, zb_movement, zb_direction):
     total_movement_algorithm_two = 0
     while zb_position != energy_position:
-        zb_position += zb_direction
-        total_movement_algorithm_two += zb_movement
-        if zb_position == -1000 or zb_position == 1000:
-            zb_direction = -(zb_direction)      
+        for i in range (zb_movement):
+            zb_position += (zb_direction)
+            total_movement_algorithm_two += zb_movement
+            if zb_position == energy_position:
+                break
+        if zb_position == energy_position:
+            break
+        zb_direction = -(zb_direction)
+        zb_movement += 500  
     return total_movement_algorithm_two
 
 def main():
     algorith_one_list = []
     algorith_two_list = []
     for i in range(-1000, 1001):
-        #initial values, units in meters
-        energy_position = i
-        zb_movement = 1
-        zb_position = 0
-        
-        total_movement_algorithm_one = algorithm_one(energy_position, zb_position, zb_movement)
-        
-        total_movement_algorithm_two_positive_first = algorithm_two_positive_first(energy_position, zb_position, zb_movement)
-        total_movement_algorithm_two_negative_first = algorithm_two_negative_first(energy_position, zb_position, zb_movement)
-        
-        total_movement_algorithm_two = int(.5 * (total_movement_algorithm_two_positive_first + total_movement_algorithm_two_negative_first))
-        
-        print("Energy position:", energy_position, "m", "   Algorithm One:", total_movement_algorithm_one, "m", "   Algorithm Two*", total_movement_algorithm_two, "m", "   *average rounded to nearest meter")
+        total_movement_algorithm_one = algorithm_one(i, 0, 250, 1)
+        total_movement_algorithm_two = algorithm_two(i, 0, 500, 1)
+
+        print("Energy position:", i, "m", "   Algorithm One:", total_movement_algorithm_one, "m", "   Algorithm Two", total_movement_algorithm_two, "m")
         
         algorith_one_list.append(total_movement_algorithm_one)
         algorith_two_list.append(total_movement_algorithm_two)
@@ -55,3 +42,4 @@ def main():
     print(algorith_two_list)
     
 main()
+input()
